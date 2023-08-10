305d34d0aa57539efa6b9385c6c9af0d277b6819
Amir Hassan <amirhassan.hamzan@gmail.com>
2023-08-10 20:37:12 +0800

    Configure css and icon template using primevue

--- src/main.js
+++ src/main.js
@@ -2,6 +2,11 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import PrimeVue from 'primevue/config'
+import "primevue/resources/themes/lara-dark-purple/theme.css"
+import "primeicons/primeicons.css"
+
+import Button from "primevue/button"
 
 import App from './App.vue'
 import router from './router'
@@ -10,5 +15,8 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(PrimeVue)
+
+app.component('Button', Button)
 
 app.mount('#app')
